5d931724989b1d6a7faddc3f7f4008d6a4223659
panpan <minp@jumei.com>
2019-03-13 17:27:37 +0800

    配置静态资源路径

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {host: 'http://localhost:8088'});
 });
 
 
