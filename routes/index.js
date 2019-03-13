92339e1785187a54d255ae5888049b1a8aa215cb
panpan <minp@jumei.com>
2019-03-13 18:16:40 +0800

    更改cdn结点

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {host: 'http://localhost:8088'});
+  res.render('index', {host: 'http://www.xpanpan.com:8088'});
 });
 
 
