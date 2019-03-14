a2a009c6c6226d75d99a7cdbaeb64a26f4d5291c
panpan <minp@jumei.com>
2019-03-14 15:11:32 +0800

    change static

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {host: 'http://static.xpanpan.com:8088'});
+  res.render('index', {host: 'https://static.xpanpan.com/webstatic'});
 });
 
 
