74fddd6059230538b5784b6f8735903f85e6922f
panpan <minp@jumei.com>
2019-03-14 14:15:27 +0800

    更改静态资源地址

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {host: 'http://www.xpanpan.com:8088'});
+  res.render('index', {host: 'http://static.xpanpan.com:8088'});
 });
 
 
